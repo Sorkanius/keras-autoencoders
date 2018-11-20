6a74c648176f02c0c56a857dcda1623eb30150cf
ignacio <ignacio@peletier.com>
2018-11-20 15:51:57 +0100

    Autoencoder finished

--- autoencoder.py
+++ autoencoder.py
@@ -1,6 +1,9 @@
 from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D
 from keras.models import Model
 from keras import backend as K
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
 
 input_img = Input(shape=(28, 28, 1))  # adapt this if using `channels_first` image data format
 
@@ -21,6 +24,7 @@ x = Conv2D(16, (3, 3), activation='relu')(x)
 x = UpSampling2D((2, 2))(x)
 decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
 
+encoder = Model(input_img, encoded)
 autoencoder = Model(input_img, decoded)
 autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
 
@@ -37,17 +41,17 @@ x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))
 from keras.callbacks import TensorBoard
 
 autoencoder.fit(x_train, x_train,
-                epochs=50,
+                epochs=2,
                 batch_size=128,
                 shuffle=True,
                 validation_data=(x_test, x_test),
-                callbacks=[TensorBoard(log_dir='/tmp/autoencoder')])
+                callbacks=[TensorBoard(log_dir='tmp/autoencoder')])
 
 decoded_imgs = autoencoder.predict(x_test)
 
 n = 10
 plt.figure(figsize=(20, 4))
-for i in range(n):
+for i in range(1, n + 1):
     # display original
     ax = plt.subplot(2, n, i)
     plt.imshow(x_test[i].reshape(28, 28))
@@ -61,15 +65,16 @@ for i in range(n):
     plt.gray()
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
-plt.show()
+plt.savefig('ae_original')
+
+encoded_imgs = encoder.predict(x_test)
 
 n = 10
 plt.figure(figsize=(20, 8))
-for i in range(n):
+for i in range(1, n + 1):
     ax = plt.subplot(1, n, i)
     plt.imshow(encoded_imgs[i].reshape(4, 4 * 8).T)
     plt.gray()
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
-plt.show()
-
+plt.savefig('ae_original_codings')
