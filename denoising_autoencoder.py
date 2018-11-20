7e617ec3c425bbc2cdcd2cc0c8c83308c0568a2b
ignacio <ignacio@peletier.com>
2018-11-20 16:32:15 +0100

    Variational autoencoder finished

--- denoising_autoencoder.py
+++ denoising_autoencoder.py
@@ -13,8 +13,8 @@ from keras.callbacks import TensorBoard
 
 x_train = x_train.astype('float32') / 255.
 x_test = x_test.astype('float32') / 255.
-x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))  # adapt this if using `channels_first` image data format
-x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))  # adapt this if using `channels_first` image data format
+x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))
+x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))
 
 noise_factor = 0.5
 x_train_noisy = x_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape) 
@@ -23,14 +23,14 @@ x_test_noisy = x_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size
 x_train_noisy = np.clip(x_train_noisy, 0., 1.)
 x_test_noisy = np.clip(x_test_noisy, 0., 1.)
 
-input_img = Input(shape=(28, 28, 1))  # adapt this if using `channels_first` image data format
+input_img = Input(shape=(28, 28, 1))
 
 x = Conv2D(32, (3, 3), activation='relu', padding='same')(input_img)
 x = MaxPooling2D((2, 2), padding='same')(x)
 x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
 encoded = MaxPooling2D((2, 2), padding='same')(x)
 
-# at this point the representation is (7, 7, 32)
+# at this point the representation is (7, 7, 32), 1568-dimensional 
 
 x = Conv2D(32, (3, 3), activation='relu', padding='same')(encoded)
 x = UpSampling2D((2, 2))(x)
